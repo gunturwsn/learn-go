0f55164be327f14d05825ba1a249605282295b0f
gunturwsn <gunturw79@gmail.com>
2024-01-23 21:25:30 +0700

    Day 15 learn go - mock

---
+++ 3_learn-go-unit-test/service/category_service_test.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"learn-go-unit-test/entity"
+	"learn-go-unit-test/repository"
+	"testing"
+)
+
+/* running in package service 'go test -v' */
+
+var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
+var categoryService = CategoryService{Repository: categoryRepository}
+
+func TestCategoryService_GetNotFound(t *testing.T) {
+	//program mock
+	categoryRepository.Mock.On("FindById", "1").Return(nil)
+	category, err := categoryService.Get("1")
+	assert.Nil(t, category)
+	assert.NotNil(t, err)
+}
+
+func TestCategoryService_GetSuccess(t *testing.T) {
+	category := entity.Category{
+		Id:   "2",
+		Name: "Laptop",
+	}
+
+	categoryRepository.Mock.On("FindById", "2").Return(category)
+	//categoryRepository.Mock.On("FindById", "2").Return(nil)
+	result, err := categoryService.Get("2")
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, category.Id, result.Id)
+	assert.Equal(t, category.Name, result.Name)
+}
