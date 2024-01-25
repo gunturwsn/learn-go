9c682b74c4887e040fb2b4e2f55272e242d6b5a3
gunturwsn <gunturw79@gmail.com>
2024-01-25 22:13:55 +0700

    Day 16 learn go - create channel

---
+++ 4_learn-go-goroutines/channel_test.go
@@ -0,0 +1,20 @@
+package learn_go_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Hello World"
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+}
