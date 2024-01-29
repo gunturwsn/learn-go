8d5739da195bddd174a407334ed3c7ece78098d3
gunturwsn <gunturw79@gmail.com>
2024-01-30 00:42:47 +0700

    Day 21 learn go - context with cancel

--- 5_learn-go-context/context_test.go
+++ 5_learn-go-context/context_test.go
@@ -3,7 +3,9 @@ package learn_go_context
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -39,4 +41,70 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("c"))
 	fmt.Println(contextF.Value("b"))
 	fmt.Println(contextA.Value("b"))
+}
+
+func TestContextWithCancelLeaks(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	destination := CreateCounterLeaks()
+	for num := range destination {
+		fmt.Println("Counter", num)
+		if num == 10 {
+			break
+		}
+	}
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+/* this function can cause goroutine leaks */
+func CreateCounterLeaks() chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounterWithContext(ctx)
+	for num := range destination {
+		fmt.Println("Counter", num)
+		if num == 10 {
+			break
+		}
+	}
+	cancel() // send cancel signal to context
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
+
+/* this function prevents goroutine leaks with context */
+func CreateCounterWithContext(ctx context.Context) chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+	return destination
 }
