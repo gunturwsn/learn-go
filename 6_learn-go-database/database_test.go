d38b0c5e7955b600895e3d3b000355c9956a09a5
gunturwsn <gunturw79@gmail.com>
2024-01-31 01:08:50 +0700

    Day 22 learn go - create and open a connection to the database

--- 6_learn-go-database/database_test.go
+++ 6_learn-go-database/database_test.go
@@ -1,10 +1,22 @@
 package learn_go_database
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"testing"
 )
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	/* username:password@tcp(host:port)/dbname?param=value */
+	db, err := sql.Open("mysql", "root:Guntur123!@tcp(localhost:3306)/learn_go_database")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// using db
 }
