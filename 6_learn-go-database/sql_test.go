9c42ec34c2a385598bef54d9246078a6b8739ed6
gunturwsn <gunturw79@gmail.com>
2024-02-01 00:33:31 +0700

    Day 23 learn go - execute sql commands

---
+++ 6_learn-go-database/sql_test.go
@@ -0,0 +1,22 @@
+package learn_go_database
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES('budi', 'Budi')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
