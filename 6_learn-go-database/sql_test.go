c6b3d23e715a140f146083ba82f1ebe286e0cfd6
gunturwsn <gunturw79@gmail.com>
2024-02-02 22:55:27 +0700

    Day 24 learn go - sql injection

--- 6_learn-go-database/sql_test.go
+++ 6_learn-go-database/sql_test.go
@@ -90,3 +90,33 @@ func TestQuerySqlComplex(t *testing.T) {
 		fmt.Println("=====================")
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #" // -> SQL Injection
+	password := "false"
+
+	script := "SELECT username FROM user WHERE username = '" + username + "' and password = '" + password + "' LIMIT 1"
+	fmt.Println(script)
+
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Succes Login", username)
+	} else {
+		fmt.Println("Login Error")
+	}
+}
