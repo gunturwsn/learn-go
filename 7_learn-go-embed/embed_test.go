cd049b1b977788ee961b55cc6d78e432e640b1e3
gunturwsn <gunturw79@gmail.com>
2024-02-06 22:10:27 +0700

    Day 26 learn go - embed file to []byte

--- 7_learn-go-embed/embed_test.go
+++ 7_learn-go-embed/embed_test.go
@@ -3,6 +3,8 @@ package learn_go_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -15,4 +17,14 @@ var version2 string
 func TestString(t *testing.T) {
 	fmt.Println(version)
 	fmt.Println(version2)
+}
+
+//go:embed thunder.png
+var logos []byte
+
+func TestByteArray(t *testing.T) {
+	err := ioutil.WriteFile("thunder_new.png", logos, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
