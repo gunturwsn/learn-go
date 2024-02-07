b6e351c878bce030c5abfa4faa630d82c5388a01
gunturwsn <gunturw79@gmail.com>
2024-02-07 22:51:08 +0700

    Day 27 learn go - servemux

--- 8_learn-go-web/handler_test.go
+++ 8_learn-go-web/handler_test.go
@@ -21,3 +21,28 @@ func TestHandler(t *testing.T) {
 	}
 	/* run http://localhost:8080/ */
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	})
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi")
+	})
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Images")
+	})
+	mux.HandleFunc("/images/thumbnails/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Thumbnails")
+	})
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+	/* run http://localhost:8080/ */
+}
