64d0a71808c2076832c3867af090354056b06bd6
gunturwsn <gunturw79@gmail.com>
2024-02-08 23:14:27 +0700

    Day 28 learn go - query parameter

---
+++ 8_learn-go-web/query_param_test.go
@@ -0,0 +1,74 @@
+package learn_go_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?name=Budi", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func MultipleParameter(writer http.ResponseWriter, request *http.Request) {
+	firstName := request.URL.Query().Get("first_name")
+	lastName := request.URL.Query().Get("last_name")
+	fmt.Fprintf(writer, "%s %s", firstName, lastName)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?first_name=Budi&last_name=Yaya", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func MultipleParameterValues(writer http.ResponseWriter, request *http.Request) {
+	var query url.Values = request.URL.Query()
+	var names []string = query["name"]
+	fmt.Fprintln(writer, strings.Join(names, ", "))
+}
+
+func TestMultipleQueryParameterValues(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?name=Budi&name=Hari&name=Yaya", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleParameterValues(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
