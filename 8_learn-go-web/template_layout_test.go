9f3af996765dddc4a56528e507fc6be9552bb188
gunturwsn <gunturw79@gmail.com>
2024-02-10 21:34:34 +0700

    Day 30 learn go - template layout

---
+++ 8_learn-go-web/template_layout_test.go
@@ -0,0 +1,31 @@
+package learn_go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateLayout(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"./templates/header.gohtml",
+		"./templates/footer.gohtml",
+		"./templates/layout.gohtml"))
+	t.ExecuteTemplate(writer, "layout", Page{
+		Title: "Template Layout",
+		Name:  "Guntur",
+	})
+}
+
+func TestTemplateLayout(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
