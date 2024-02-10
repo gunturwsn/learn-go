39e5c3e04b63ad5585c0813661aa5e251f009db5
gunturwsn <gunturw79@gmail.com>
2024-02-10 21:09:12 +0700

    Day 30 learn go - template action

---
+++ 8_learn-go-web/template_action_test.go
@@ -0,0 +1,86 @@
+package learn_go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateActionIf(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
+	t.ExecuteTemplate(writer, "if.gohtml", Page{
+		Title: "Template Action",
+		//Name:  "Guntur",
+	})
+}
+
+func TestTemplateActionIf(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionIf(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionOperator(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/comparator.gohtml"))
+	t.ExecuteTemplate(writer, "comparator.gohtml", map[string]interface{}{
+		"FinalValue": 90,
+	})
+}
+
+func TestTemplateActionOperator(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionOperator(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionRange(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/range.gohtml"))
+	t.ExecuteTemplate(writer, "range.gohtml", map[string]interface{}{
+		"Hoobies": []string{
+			"Running", "Reading", "Swimming",
+		},
+	})
+}
+
+func TestTemplateActionRange(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateActionWith(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/address.gohtml"))
+	t.ExecuteTemplate(writer, "address.gohtml", map[string]interface{}{
+		"Title": "Template Action With",
+		"Name":  "Guntur",
+		"Address": map[string]interface{}{
+			"Street": "Jalan Baru",
+			"City":   "Jakarta",
+		},
+	})
+}
+
+func TestTemplateActionWith(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionWith(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
