f20570f5264e4bd92fef0ee3258f0d0c1694a74e
gunturwsn <gunturw79@gmail.com>
2024-02-13 22:31:08 +0700

    Day 33 learn go - not found handler

---
+++ 9_learn-go-httprouter/not_found_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNotFound(t *testing.T) {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Not Found")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Not Found", string(body))
+}
