d93ebd665d93429349f6bfd223cb01e8f7412c61
gunturwsn <gunturw79@gmail.com>
2024-02-17 09:07:39 +0700

    Day 37 learn go - streaming decoder

---
+++ 10_learn-go-json/decoder_test.go
@@ -0,0 +1,20 @@
+package learn_go_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestStreamDecoder(t *testing.T) {
+	reader, _ := os.Open("Customer.json")
+	decoder := json.NewDecoder(reader)
+
+	customer := &Customer{}
+	err := decoder.Decode(customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+}
