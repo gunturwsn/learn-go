3f6b5d2e2c94a485f3c6bd63f4e68f48bfed2bb9
gunturwsn <gunturw79@gmail.com>
2024-02-20 22:15:29 +0700

    Day 40 learn go - http server

--- 11_learn-go-restful-api/main.go
+++ 11_learn-go-restful-api/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 	"learn-go-restful-api/app"
 	"learn-go-restful-api/controller"
+	"learn-go-restful-api/helper"
 	"learn-go-restful-api/repository"
 	"learn-go-restful-api/service"
+	"net/http"
 )
 
 func main() {
@@ -22,4 +25,12 @@ func main() {
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 }
