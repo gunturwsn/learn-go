21f9c823103f4673ab79f434afb874d81f2de6fe
gunturwsn <gunturw79@gmail.com>
2024-02-22 23:49:48 +0700

    Day 42 learn go - unit test

--- 11_learn-go-restful-api/main.go
+++ 11_learn-go-restful-api/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 	"learn-go-restful-api/app"
 	"learn-go-restful-api/controller"
-	"learn-go-restful-api/exception"
 	"learn-go-restful-api/helper"
 	"learn-go-restful-api/middleware"
 	"learn-go-restful-api/repository"
@@ -20,15 +18,7 @@ func main() {
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
