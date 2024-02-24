47e3bf87463ef56f5c5177af0d6b277cb344ab97
gunturwsn <gunturw79@gmail.com>
2024-02-24 18:56:34 +0700

    Day 43 learn go - dependency injection

---
+++ 12_learn-go-dependency-injection/test/simple_service_test.go
@@ -0,0 +1,12 @@
+package test
+
+import (
+	"fmt"
+	"learn-go-dependency-injection/simple"
+	"testing"
+)
+
+func TestSimpleService(t *testing.T) {
+	simpleService := simple.InitializedService()
+	fmt.Println(simpleService.SimpleRepository)
+}
